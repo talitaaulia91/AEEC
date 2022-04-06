37df95ab2f9da7631048b8fa4585d9f6aa905b30
talitaaulia91 <90740632+talitaaulia91@users.noreply.github.com>
2022-04-06 12:40:41 +0700

    first

---
+++ client/peserta.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table Datatable Jquery - Mazer Admin Dashboard</title>
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    
+<!-- <link rel="stylesheet" href="assets/vendors/jquery-datatables/jquery.dataTables.min.css"> -->
+<link rel="stylesheet" href="../assets/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="../assets/vendors/fontawesome/all.min.css">
+<style>
+    table.dataTable td{
+        padding: 15px 8px;
+    }
+    .fontawesome-icons .the-icon svg {
+        font-size: 24px;
+    }
+</style>
+
+    <link rel="stylesheet" href="../assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="../assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="../assets/css/app.css">
+    <link rel="shortcut icon" href="../assets/images/favicon.svg" type="image/x-icon">
+</head>
+
+<body>
+
+<?php include_once('../sidebar/sidebar.php'); ?>
+
+        </ul>
+    </div>
+    <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+</div>
+        </div>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+            
+<div class="page-heading">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>DataTable Jquery</h3>
+                <p class="text-subtitle text-muted">For user to check they list</p>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">DataTable Jquery</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <!-- Basic Tables start -->
+    <section class="section">
+        <div class="card">
+            <div class="card-header">
+                Jquery Datatable
+            </div>
+            <div class="card-body">
+                <table class="table" id="table1">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>City</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Graiden</td>
+                            <td>vehicula.aliquet@semconsequat.co.uk</td>
+                            <td>076 4820 8838</td>
+                            <td>Offenburg</td>
+                            <td>
+                                <span class="badge bg-success">Active</span>
+                            </td>
+                        </tr>
+                       
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </section>
+    <!-- Basic Tables end -->
+</div>
+
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2021 &copy; Mazer</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                                href="http://ahmadsaugi.com">A. Saugi</a></p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="../assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    
+<script src="../assets/vendors/jquery/jquery.min.js"></script>
+<script src="../assets/vendors/jquery-datatables/jquery.dataTables.min.js"></script>
+<script src="../assets/vendors/jquery-datatables/custom.jquery.dataTables.bootstrap5.min.js"></script>
+<script src="../assets/vendors/fontawesome/all.min.js"></script>
+<script>
+    // Jquery Datatable
+    let jquery_datatable = $("#table1").DataTable()
+</script>
+
+    <script src="../assets/js/mazer.js"></script>
+</body>
+
+</html>
---
+++ sidebar/sidebar.php
@@ -0,0 +1,141 @@
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+    </nav>
+    <div id="app">
+        <div id="sidebar" class="active">
+            <div class="sidebar-wrapper active">
+    <div class="sidebar-header">
+        <div class="d-flex justify-content-between">
+            <div class="logo">
+                <a href="dashboard.php"><img src="../assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+            </div>
+            <div class="toggler">
+                <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="sidebar-menu">
+        <ul class="menu">
+            <li class="sidebar-title">Menu</li>
+            
+            <li
+                class="sidebar-item  <?= !!strpos($_SERVER['REQUEST_URI'], 'dashboard') ? 'active' : '' ?> ">
+                <a href="../layout/dashboard.php" class='sidebar-link '>
+                    <i class="bi bi-grid-fill"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+          
+            <li
+                class="sidebar-item  <?= !!strpos($_SERVER['REQUEST_URI'], 'peserta') ? 'active' : '' ?>">
+                <a href="../client/peserta.php" class='sidebar-link '>
+                    <i class="bi bi-grid-1x2-fill"></i>
+                    <span>Peserta</span>
+                </a>
+            </li>
+     
+            <li
+                class="sidebar-item  has-sub ">
+                <a href="#" class='sidebar-link '>
+                    <i class="bi bi-grid-1x2-fill"></i>
+                    <span>Course</span>
+                </a>
+                <ul class="submenu ">
+                    <li class="submenu-item  <?= !!strpos($_SERVER['REQUEST_URI'], 'reguler') ? 'active' : '' ?>">
+                        <a href="../course/reguler.php">Reguler Class</a>
+                    </li>
+                    <li class="submenu-item  <?= !!strpos($_SERVER['REQUEST_URI'], 'in-house') ? 'active' : '' ?>">
+                        <a href="../course/in-house.php">In-House Training</a>
+                    </li>
+                    <li class="submenu-item  <?= !!strpos($_SERVER['REQUEST_URI'], 'c-suite') ? 'active' : '' ?>">
+                        <a href="../course/c-suite.php">C-Suite connection</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li
+                class="sidebar-item  <?= !!strpos($_SERVER['REQUEST_URI'], 'discount') ? 'active' : '' ?> ">
+                <a href="../discount/discount.php" class='sidebar-link '>
+                    <i class="bi bi-grid-1x2-fill"></i>
+                    <span>Diskon</span>
+                </a>
+            </li>
+
+
+
+            <li
+                class="sidebar-item  <?= !!strpos($_SERVER['REQUEST_URI'], 'pendaftaran') ? 'active' : '' ?>">
+                <a href="../pendaftaran/pendaftaran.php" class='sidebar-link '>
+                    <i class="bi bi-file-earmark-medical-fill"></i>
+                    <span>Pendaftaran</span>
+                </a>
+            </li>
+
+
+            <li
+                class="sidebar-item <?= !!strpos($_SERVER['REQUEST_URI'], 'pembayaran') ? 'active' : '' ?>">
+                <a href="../pembayaran/pembayaran.php" class='sidebar-link  '>
+                    <i class="bi bi-file-earmark-medical-fill"></i>
+                    <span>Pembayaran</span>
+                </a>
+            </li>
+
+            
+                   
+            <li class="sidebar-title">Forms &amp; Tables</li>
+            
+            <li
+                class="sidebar-item  has-sub">
+                <a href="#" class='sidebar-link'>
+                    <i class="bi bi-hexagon-fill"></i>
+                    <span>Form Elements</span>
+                </a>
+                <ul class="submenu ">
+                    <li class="submenu-item ">
+                        <a href="form-element-input.php">Input</a>
+                    </li>
+                    <li class="submenu-item ">
+                        <a href="form-element-input-group.php">Input Group</a>
+                    </li>
+                    <li class="submenu-item ">
+                        <a href="form-element-select.php">Select</a>
+                    </li>
+                    <li class="submenu-item ">
+                        <a href="form-element-radio.php">Radio</a>
+                    </li>
+                    <li class="submenu-item ">
+                        <a href="form-element-checkbox.php">Checkbox</a>
+                    </li>
+                    <li class="submenu-item ">
+                        <a href="form-element-textarea.php">Textarea</a>
+                    </li>
+                </ul>
+            </li>
+            
+            <li
+                class="sidebar-item  ">
+                <a href="form-layout.php" class='sidebar-link'>
+                    <i class="bi bi-file-earmark-medical-fill"></i>
+                    <span>Form Layout</span>
+                </a>
+            </li>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- /.sidebar-menu -->
+  </div>
