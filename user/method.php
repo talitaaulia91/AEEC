2564b7d1ff28ecbe1d958c92ec4e3d38ba0a33aa
DikaNizzer <dikanizzer28@gmail.com>
2022-04-07 13:24:47 +0700

    query DB

---
+++ user/method.php
@@ -0,0 +1,24 @@
+<?php
+
+//var untuk nyambung ke db
+$koneksi = mysqli_connect("localhost", "root", "", "uas");
+
+
+//Function untuk kuery db dan mengembalikan hasilnya berupa array
+function query($query){
+    global $koneksi;
+
+    $hasil = mysqli_query($koneksi, $query); //membuat query mysql
+    $data = [];
+
+    while($sql = mysqli_fetch_assoc($hasil) ) { //mengambil data di mysql
+        $data[] = $sql;
+    }
+
+    return  $data;
+}
+
+
+
+
+?>
