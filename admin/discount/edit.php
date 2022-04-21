d983bb2c7461813babeff44bf1bf3001579be557
annisaaris <annisa.aristawati-2020@vokasi.unair.ac.id>
2022-04-21 11:39:36 +0700

    update edit

--- admin/discount/edit.php
+++ admin/discount/edit.php
@@ -125,31 +125,23 @@ include_once('../../config/database.php');
         }
     ?>
 
-
-
-
-
-
-
     <!-- Basic Tables end -->
 </div>
 
-            <footer>
-                <div class="footer clearfix mb-0 text-muted">
-                    <div class="float-start">
-                        <p>2021 &copy; Mazer</p>
-                    </div>
-                    <div class="float-end">
-                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                                href="http://ahmadsaugi.com">A. Saugi</a></p>
-                    </div>
-                </div>
-            </footer>
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2021 &copy; Mazer</p>
+        </div>
+        <div class="float-end">
+            <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a href="http://ahmadsaugi.com">A. Saugi</a></p>
         </div>
     </div>
-    <script src="../../assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="../../assets/js/bootstrap.bundle.min.js"></script>
-    
+</footer>
+</div>
+</div>
+<script src="../../assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="../../assets/js/bootstrap.bundle.min.js"></script>  
 <script src="../../assets/vendors/jquery/jquery.min.js"></script>
 <script src="../../assets/vendors/jquery-datatables/jquery.dataTables.min.js"></script>
 <script src="../../assets/vendors/jquery-datatables/custom.jquery.dataTables.bootstrap5.min.js"></script>
